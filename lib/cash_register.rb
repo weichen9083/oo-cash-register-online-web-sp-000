39955b81e791ea2fe46f0c01eff4d8698202e134
wei chen <weichen9083@gmail.com>
2020-12-02 20:40:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,19 +7,23 @@ class CashRegister
   def initialize(discount = 0)
     self.total = 0
     self.discount = discount
-    
   end 
+  
+  
   
   def discount 
     @discount
   end 
+  
+  
+  
   
   def add_item(title, price,quantity = 1)
     added_price = price * quantity
     self.total +=added_price
   end 
     
-end 
+
 
   
   def apply_discount
@@ -28,6 +32,7 @@ end
     off = self.discount / 100.to_f
     self.total = self.total - (self.total * off)
     "After the discount, the total comes to $#{self.total.to_i}."
+  end 
   end 
   
   
